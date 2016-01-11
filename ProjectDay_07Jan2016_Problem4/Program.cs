2d6f93ded56da91367d5bbc22c8dccbc1d6c6377
R-Richard <richraponi@gmail.com>
2016-01-11 00:49:22 -0500

    Problem4_Commit

---
+++ ProjectDay_07Jan2016_Problem4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDay_07Jan2016_Problem4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"Welcome to We Can Code IT\"");
+        }
+    }
+}
